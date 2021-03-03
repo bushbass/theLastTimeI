999edff162d7c667811824708c27d4bb378769f4
Alex Nielsen <alex@bigal.org>
2021-03-03 13:02:09 -0500

    added illustration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './styles.css';
 import { v4 as uuidv4 } from 'uuid';
+import { ReactComponent as Logo } from './question.svg';
 
 export default function App() {
   // master list
@@ -60,70 +61,48 @@ export default function App() {
   }, [events]);
 
   return (
-    <div className='App'>
-      <h1>The last time I ...</h1>
-      <form>
-        <label htmlFor='currentEvent'>Event</label>
-        <input
-          placeholder='add an event'
-          id='currentEvent'
-          type='text'
-          value={currentEvent}
-          onChange={(event) => setCurrentEvent(event.target.value)}
-        />
+    <div className="App">
+      <div className="container">
+        <Logo />
+        <h1>The last time I ...</h1>
+        <form>
+          <label htmlFor="currentEvent">Event</label>
+          <input
+            placeholder="add an event"
+            id="currentEvent"
+            type="text"
+            value={currentEvent}
+            onChange={(event) => setCurrentEvent(event.target.value)}
+          />
+          <br />
+          <label htmlFor="date">Date</label>
+          <input
+            id="date"
+            type="date"
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+          <button onClick={submitEvent}>Add event</button>
+        </form>
+        <button onClick={sortOldest}>Oldest first</button>
+        <button onClick={sortNewest}>Newest first</button>
         <br />
-        <label htmlFor='date'>Date</label>
         <input
-          id='date'
-          type='date'
-          value={date}
-          onChange={(event) => setDate(event.target.value)}
+          type="text"
+          placeholder="filter events"
+          onChange={(event) => setFilterTerm(event.target.value)}
+          value={filterTerm}
         />
-        <button onClick={submitEvent}>Add event</button>
-      </form>
-      <button onClick={sortOldest}>Oldest first</button>
-      <button onClick={sortNewest}>Newest first</button>
-      <br />
-      <input
-        type='text'
-        placeholder='filter events'
-        onChange={(event) => setFilterTerm(event.target.value)}
-        value={filterTerm}
-      />
-      <button onClick={() => setFilterTerm('')}>Clear filter</button>
+        <button onClick={() => setFilterTerm('')}>Clear filter</button>
 
-      {filterTerm === ''
-        ? events.map((eachEvent) => {
-            return (
-              <div key={eachEvent.id}>
-                <p>
-                  {eachEvent.currentEvent} -{eachEvent.date}
-                  <button
-                    className='deleteButton'
-                    onClick={() => {
-                      setEvents(
-                        events.filter((event) => event.id !== eachEvent.id)
-                      );
-                    }}
-                  >
-                    X
-                  </button>
-                </p>
-                <hr />
-              </div>
-            );
-          })
-        : events
-            .filter((item) =>
-              item.currentEvent.toLowerCase().includes(filterTerm.toLowerCase())
-            )
-            .map((eachEvent) => {
+        {filterTerm === ''
+          ? events.map((eachEvent) => {
               return (
                 <div key={eachEvent.id}>
                   <p>
                     {eachEvent.currentEvent} -{eachEvent.date}
                     <button
-                      className='deleteButton'
+                      className="deleteButton"
                       onClick={() => {
                         setEvents(
                           events.filter((event) => event.id !== eachEvent.id)
@@ -136,7 +115,37 @@ export default function App() {
                   <hr />
                 </div>
               );
-            })}
+            })
+          : events
+              .filter((item) =>
+                item.currentEvent
+                  .toLowerCase()
+                  .includes(filterTerm.toLowerCase())
+              )
+              .map((eachEvent) => {
+                return (
+                  <div key={eachEvent.id}>
+                    <p>
+                      {eachEvent.currentEvent} -{eachEvent.date}
+                      <button
+                        className="deleteButton"
+                        onClick={() => {
+                          setEvents(
+                            events.filter((event) => event.id !== eachEvent.id)
+                          );
+                        }}
+                      >
+                        X
+                      </button>
+                    </p>
+                    <hr />
+                  </div>
+                );
+              })}
+        <p>
+          <a href="https://storyset.com/">Illustration by Freepik Storyset</a>
+        </p>
+      </div>
     </div>
   );
 }
